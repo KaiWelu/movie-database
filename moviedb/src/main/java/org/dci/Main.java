78958c970d5479d6dce0b5c47119a9277945b868
KaiWelu <kaiweluda@gmail.com>
2025-03-18 14:43:46 +0100

    adds javalin

--- moviedb/src/main/java/org/dci/Main.java
+++ moviedb/src/main/java/org/dci/Main.java
@@ -1,5 +1,6 @@
 package org.dci;
 
+import io.javalin.Javalin;
 import org.dci.Data.DbConnection;
 import org.dci.Models.Movie;
 
@@ -9,11 +10,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+
+        // getting all movies from the database
         DbConnection dbConnection = new DbConnection();
         ArrayList<Movie> movies = dbConnection.getAllMovies();
 
         for(Movie movie : movies) {
             System.out.println(movie.getTitle() + "\n");
         }
+
+        var app = Javalin.create()
+                         .get("/", ctx -> ctx.result("Welcome to MovieDB"))
+                         .start(7070);
+
+        app.get("/hello", ctx -> ctx.result("This is the hello route"));
+
+
     }
 }
